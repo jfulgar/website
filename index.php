e2f7ed1361cb25d089fdbab54dc948cb7c6b6ba3
Keo Strife <keo_strife@yahoo.com>
2013-05-15 13:17:39 -0400

    v0.0.3
    Basic Blog functionality

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
-	define('BASE','/125');
+	define('BASE','/jfulgar');
 	include('application/core/keo.strife');
-	//include('application/core/connect.strife');
+	include('application/core/connect.strife');
 	include('application/models/main.model');
 	
-
+	//default title
+	$strife_title = 'Jfulgar Portfolio';
 
 	/*route with base directory.
 	 *Everything else to 404.php
@@ -15,73 +16,92 @@
 	$strife_request = explode('/',$strife_path);
 
 	//===initialize
-	if(method_exists($keo,'init')) $keo->init();
+	if(method_exists($jeff,'init')) $jeff->init();
 
 	//1st routing floor handling
-	if($keo->getRouteFloor($strife_request) < 2){
+	if($jeff->getRouteFloor($strife_request) < 2){
 		if($strife_request[1] == 'index.html' || 
 		  	$strife_request[1] == '' || 
 			$strife_request[1] == 'index.php' ||
 			$strife_request[1] == 'home') 
 		{
-			$strife_title = 'Home';
-			include('application/templates/home_head.template');
-			include('application/views/home.view');
-			include('application/templates/foot.template');
+			$jeff->cachePage('cache/home.html',function(){
+				$strife_title = 'Home';
+				include('application/templates/home_head.template');
+				include('application/views/home.view');
+				include('application/templates/foot.template');
+			},60*30);
 		/**/
 		} else if ($strife_request[1] == 'about') {
-			
+			$jeff->cachePage('cache/about.html',function(){
 				$strife_title = 'Strife || About';
 				include('application/templates/head.template');
 				include('application/views/about.view');
 				include('application/templates/foot.template');
+			},60*30);
 		} else if ($strife_request[1] == 'portfolio') {
-			
+			$jeff->cachePage('cache/portfolio.html',function(){
 				$strife_title = 'Strife || Portfolio';
 				include('application/templates/head.template');
 				include('application/views/portfolio.view');
 				include('application/templates/foot.template');
+			},60*30);
 		} else if ($strife_request[1] == 'contact') {
-			
+			$jeff->cachePage('cache/contact.html',function(){
 				$strife_title = 'Strife || Contact';
 				include('application/templates/head.template');
 				include('application/views/contact.view');
 				include('application/templates/foot.template');
+			},60*30);
 		} else if ($strife_request[1] == 'gallery') {
-			
+			$jeff->cachePage('cache/gallery.html',function(){
 				$strife_title = 'Gallery';
 				include('application/templates/head.template');
 				include('application/views/gallery.view');
 				include('application/templates/foot.template');	
+			},60*30);
 		} else {
-			$keo->cachePage('cache/404.html',function(){
+			$jeff->cachePage('cache/404.html',function(){
 				$strife_title = 'Strife || 404';
 				include('application/templates/head.template');
 				include('application/views/404.view');
 				include('application/templates/foot.template');
-			},60*10);
+			},60*30);
 		}
-	/*
+	
 	//2nd routing floor handling
-	} else if ($keo->getRouteFloor($strife_request) == 2) {
-		if($strife_request[1] == 'home' && $strife_request[2] == 'info'){
+	} else if ($jeff->getRouteFloor($strife_request) == 2) {
+		if($strife_request[1] == 'portfolio' && 
+			is_numeric($strife_request[2]) &&
+			$strife_request[2] <= $jeff->countPost()){
 			//handling http://example.com/home/info/
+			$post = $jeff->getPost(intval($strife_request[2]));
+			if (!$post) {
+				$jeff->cachePage('cache/404.html',function(){
+					$strife_title = 'Strife || About';
+					include('application/templates/head.template');
+					include('application/views/404.view');
+					include('application/templates/foot.template');
+				},60*30);
+			} else {
+				print_r($post);
+			}
 		} else {
-			$keo->cachePage('cache/404.html',function(){
+			$jeff->cachePage('cache/404.html',function(){
 				$strife_title = 'Strife || About';
 				include('application/templates/head.template');
 				include('application/views/404.view');
 				include('application/templates/foot.template');
-			},60*10);
+			},60*30);
 		}
-	*/
+	
 	} else {
-		$keo->cachePage('cache/404.html',function(){
+		$jeff->cachePage('cache/404.html',function(){
 			$strife_title = 'Strife || 404';
 			include('application/templates/head.template');
 			include('application/views/404.view');
 			include('application/templates/foot.template');
-		},60*10);
+		},60*30);
 	}
 
 	
@@ -89,6 +109,6 @@
 /*Global variable list:
  *$db_....
  *$strife_...
- *$keo
+ *$jeff
  */
 ?>
